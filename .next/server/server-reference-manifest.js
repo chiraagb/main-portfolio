236b3fdb7a4a4b1104bc005f2bc77f04f65d8c69
Chirag Bhandakkar <chirag.b@framedx.com>
2024-09-01 16:37:47 +0530

    navbar and herosection

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sdyT7NMYhEygJy87NjonUBkGnzE3uRRwMdrO66LJc/g=\"\n}"
---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter, Calistoga, Poppins } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+});
+
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-calistoga",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "Created with the help of Frontend Tribe",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-inter"
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
