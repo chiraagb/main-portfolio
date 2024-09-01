8b40ec96dbd5fb5739b2d36557f774e28648606a
Chirag Bhandakkar <chirag.b@framedx.com>
2024-09-01 20:35:34 +0530

    hero and project section ui complete

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,48 +10,50 @@ export const HeroSection = () => {
   return (
     <>
       <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-        <div
-          className="absolute inset-0 -z-30 opacity-5"
-          style={{
-            backgroundImage: `url(${grainImage.src})`,
-          }}
-        ></div>
-        <div className="size-[620px] hero-ring"></div>
-        <div className="size-[820px] hero-ring"></div>
-        <div className="size-[1020px] hero-ring"></div>
-        <div className="size-[1220px] hero-ring"></div>
+        <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+          <div
+            className="absolute inset-0 -z-30 opacity-5"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <div className="size-[620px] hero-ring"></div>
+          <div className="size-[820px] hero-ring"></div>
+          <div className="size-[1020px] hero-ring"></div>
+          <div className="size-[1220px] hero-ring"></div>
 
-        <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-emerald-300" />
-        </HeroOrbit>
-        <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-emerald-300" />
-        </HeroOrbit>
-        <HeroOrbit size={590} rotation={98}>
-          <StarIcon className="size-8 text-emerald-300" />
-        </HeroOrbit>
+          <HeroOrbit size={800} rotation={-72}>
+            <StarIcon className="size-28 text-emerald-300" />
+          </HeroOrbit>
+          <HeroOrbit size={550} rotation={20}>
+            <StarIcon className="size-12 text-emerald-300" />
+          </HeroOrbit>
+          <HeroOrbit size={590} rotation={98}>
+            <StarIcon className="size-8 text-emerald-300" />
+          </HeroOrbit>
 
-        <HeroOrbit size={430} rotation={-14}>
-          <StarIcon className="size-8 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={440} rotation={79}>
-          <StarIcon className="size-5 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={530} rotation={178}>
-          <StarIcon className="size-10 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={710} rotation={144}>
-          <StarIcon className="size-14 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={720} rotation={85}>
-          <div className="size-3 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41}>
-          <div className="size-2 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5}>
-          <div className="size-2 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
+          <HeroOrbit size={430} rotation={-14}>
+            <StarIcon className="size-8 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={440} rotation={79}>
+            <StarIcon className="size-5 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={530} rotation={178}>
+            <StarIcon className="size-10 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={710} rotation={144}>
+            <StarIcon className="size-14 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={720} rotation={85}>
+            <div className="size-3 rounded-full text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={520} rotation={-41}>
+            <div className="size-2 rounded-full text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={650} rotation={-5}>
+            <div className="size-2 rounded-full text-emerald-300/20" />
+          </HeroOrbit>
+        </div>
 
         <div className="container">
           <div className="flex flex-col items-center">
