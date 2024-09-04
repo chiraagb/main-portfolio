7559a7e96325ceeeb8bbb5ba3334b3c00811f088
Chirag Bhandakkar <chirag.b@framedx.com>
2024-09-04 22:39:19 +0530

    hero section animations

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,29 +1,55 @@
 import React, { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  orbitDuration,
+  shouldOrbit = false,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  orbitDuration?: string;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+}>) => {
   return (
     <div>
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
         <div
-          className="/*border border-red-500*/ /*animate-spin [animation-duration:30s]*/ /*outline outline-1 outline-red-500*/ flex items-start justify-start"
+          className={twMerge(shouldOrbit === true && "animate-spin")}
           style={{
-            transform: `rotate(${rotation}deg)`,
-            height: `${size}px`,
-            width: `${size}px`,
+            animationDuration: orbitDuration,
           }}
         >
           <div
-            className="/*border border-red-500*/ inline-flex /*animate-spin [animation-duration:5s]*/ /*outline outline-1 outline-red-500*/"
+            className="/*border border-red-500*/ /*animate-spin [animation-duration:30s]*/ /*outline outline-1 outline-red-500*/ flex items-start justify-start"
             style={{
-              transform: `rotate(${rotation * -1}deg)`,
+              transform: `rotate(${rotation}deg)`,
+              height: `${size}px`,
+              width: `${size}px`,
             }}
           >
-            {/* <StarIcon className="size-28 text-emerald-300" /> */}
-            {children}
+            <div
+              className={twMerge(shouldSpin === true && "animate-spin")}
+              style={{
+                animationDuration: spinDuration,
+              }}
+            >
+              <div
+                className="/*border border-red-500*/ inline-flex /*animate-spin [animation-duration:5s]*/ /*outline outline-1 outline-red-500*/"
+                style={{
+                  transform: `rotate(${rotation * -1}deg)`,
+                }}
+              >
+                {/* <StarIcon className="size-28 text-emerald-300" /> */}
+                {children}
+              </div>
+            </div>
           </div>
         </div>
       </div>
